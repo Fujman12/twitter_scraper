1bf634f71353b9ede37066fa7eed408348882306
Narek Martikian <Fujman94@gmail.com>
2017-04-06 17:19:29 +0300

    minor changes applied to the script

--- parse.py
+++ parse.py
@@ -4,6 +4,23 @@ import sys
 
 from openpyxl import Workbook
 
+def write_to_sheet(name, number_of_followers, number_following, location, description):
+    global row_count
+
+    ws1["A%d" % row_count] = name
+    ws1["B%d" % row_count] = number_of_followers
+    ws1["C%d" % row_count] = number_following
+    ws1["D%d" % row_count] = location
+    #Link
+    ws1["E%d" % row_count] = "https://twitter.com/%s" % name
+    ws1["F%d" % row_count] = description
+
+    wb.save("result.xlsx")
+    row_count += 1
+
+
+
+
 auth = tweepy.OAuthHandler('ioVq2QrpDEreTHol2bwa7B5RT', '34zySkbtzaMS26O5vAlJKVDi9CWkxrXieJigFlZagFOxfZjFWL')
 auth.set_access_token('3361355566-L4JgCU0DQZOxZyBFgbYLNGdADOKc1B2OP6N9CRV', 'jNePLU3Iuf8l7m5yvKw97eGcEcvUBlaQGshqSrlBmwsXk')
 
@@ -26,26 +43,10 @@ ws1["F1"] = "Description"
 
 wb.save("result.xlsx")
 
-def write_to_sheet(name, number_of_followers, number_following, location, description):
-    global row_count
-
-    ws1["A%d" % row_count] = name
-    ws1["B%d" % row_count] = number_of_followers
-    ws1["C%d" % row_count] = number_following
-    ws1["D%d" % row_count] = location
-    #Link
-    ws1["E%d" % row_count] = "https://twitter.com/%s" % name
-    ws1["F%d" % row_count] = description
-
-    wb.save("result.xlsx")
-    row_count += 1
-
-
-
-
 while True:
     try:
         u = next(user)
+        print("Adding %s to sheet" % u.screen_name)
         write_to_sheet(u.screen_name, u.followers_count, u.friends_count, u.location, u.description)
 
     except :
@@ -54,5 +55,5 @@ while True:
         print('We got a timeout ... Sleeping for 15 minutes')
         time.sleep(15*60)
 
-        u = next(user)
-        write_to_sheet(u.screen_name, u.followers_count, u.friends_count, u.location, u.description)
+        #u = next(user)
+        #write_to_sheet(u.screen_name, u.followers_count, u.friends_count, u.location, u.description)
